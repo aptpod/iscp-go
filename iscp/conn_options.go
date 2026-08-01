fd12c01bfcdedca0f94f83d024d83b24a00c741e
miyauchi <miyauchi@aptpod.co.jp>
2026-08-01 18:07:51 +0900

    docs(iscp): 無制限再接続の挙動説明を修正する

--- iscp/conn_options.go
+++ iscp/conn_options.go
@@ -410,7 +410,7 @@ type MultiTransportConfig struct {
 
 	// MaxReconnectAttemptsは、再接続を試行する最大回数です。
 	// 0を指定すると既定値30が使われます。負値は無制限リトライを意味し、
-	// 接続が回復しない限り再接続ループは終了しないため、Dial / Writeは返りません（意図された仕様です）。
+	// 接続が回復しない限り再接続ループは終了しません（意図された仕様です）。
 	MaxReconnectAttempts int
 	ReconnectInterval    time.Duration
 }
--- transport/reconnect/transport.go
+++ transport/reconnect/transport.go
@@ -121,7 +121,7 @@ type DialConfig struct {
 	DialConfig transport.DialConfig
 	// MaxReconnectAttemptsは、再接続を試行する最大回数です。
 	// 0を指定すると既定値30が使われます。負値は無制限リトライを意味し、
-	// 接続が回復しない限り再接続ループは終了しないため、Dial / Writeは返りません（意図された仕様です）。
+	// 接続が回復しない限り再接続ループは終了しません（意図された仕様です）。
 	MaxReconnectAttempts int
 	ReconnectInterval    time.Duration
 	HeartbeatInterval    time.Duration
